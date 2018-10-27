5ec308e1732f740b871e260f538ab1c239690af2
Chris Y Cho <chrisycho04@gmail.com>
2018-10-26 23:23:32 -0700

    Updates

---
+++ precache-manifest.3e234cef207bee335bf95495239bd098.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ec46125784e0b1f6cca6",
+    "url": "/react-app/static/js/runtime~main.ec461257.js"
+  },
+  {
+    "revision": "b68ca69f15afd5123223",
+    "url": "/react-app/static/js/main.b68ca69f.chunk.js"
+  },
+  {
+    "revision": "bd3d8cc767d2802c73d2",
+    "url": "/react-app/static/js/1.bd3d8cc7.chunk.js"
+  },
+  {
+    "revision": "b68ca69f15afd5123223",
+    "url": "/react-app/static/css/main.32f15910.chunk.css"
+  },
+  {
+    "revision": "bd3d8cc767d2802c73d2",
+    "url": "/react-app/static/css/1.1a4853fb.chunk.css"
+  },
+  {
+    "revision": "02470e0adc151e4dbc5102157662030c",
+    "url": "/react-app/index.html"
+  }
+];
