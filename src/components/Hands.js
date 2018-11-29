ebd5da8948f65bb8faf905a88b98ad86d8cf24aa
Chris Y Cho <chrisycho04@gmail.com>
2018-11-29 00:52:30 -0800

    no longer says pg tiles

--- src/components/Hands.js
+++ src/components/Hands.js
@@ -2,13 +2,9 @@ import React from 'react';
 
 const Hands = (props) =>{
 
-    const style = {
-        fontSize: '5.0em'
-    }
-
-    return (
-        props.start ? <h1 style={style} align='center'>Pai Gow Tiles</h1> : 
-            <div align='center'>
+    return ( 
+            //knows fadeOut and fadeIn without mentioning it.
+            <div className={props.start ? 'fadeOut' : 'fadeIn'} align='center'>
                 <img src={props.hand1} alt={props.hand1}></img>
                 <img src={props.hand2} alt={props.hand2}></img>
                 <img src={props.hand3} alt={props.hand3}></img>
