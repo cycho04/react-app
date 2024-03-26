b923651d316f2b2916f8ab3034c6fef74420db89
Chris Cho <chrisycho04@gmail.com>
2024-03-25 20:25:41 -0700

    fix useEffect hydration

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -1,14 +1,24 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+
 import {TileInterface} from '../_lib/deck';
 import { dealNewHand } from '../_lib/utils';
 import TileRankCard from 'app/how-to-play/rankings/_components/TileRankCard';
 
 export default function Page(){
+
+    const [numOfHands, setNumOfHands] = useState(1)
+    const [hands, setHands] = useState<TileInterface[][]>([])
+
+    useEffect(() => {
+        setHands(dealNewHand(numOfHands));
+    }, [])
+    
     return (
         <>
             {
-                dealNewHand(1).map((hand: TileInterface[]) => {
+                hands.map((hand: TileInterface[]) => {
                     return (
                         hand.map(({name, description}: TileInterface, index) => {
                             return (
