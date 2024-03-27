8e213f58725b6b531ce179ce43ea558274886f1f
Chris Cho <chrisycho04@gmail.com>
2024-03-26 23:47:48 -0700

    added select tile functionality and test

--- app/how-to-play/rankings/_components/TileRankCard.tsx
+++ app/how-to-play/rankings/_components/TileRankCard.tsx
@@ -3,19 +3,29 @@ import Image from 'next/image';
 interface TileRankInterface{
     key: string;
     name: string;
-    description: string
+    index: number;
+    description: string,
+    selected: boolean;
+    handleTileClick: (index: number) => void
 }
 
-export default function TileRankCard({name, description}: TileRankInterface){
+export default function TileRankCard({
+    name, description, index, selected, handleTileClick
+}: TileRankInterface){
     return (
         <>
-            <Image
-                src={`/${name}.jpeg`}
-                width={100}
-                height={100}
-                alt={`Pai Gow Tile: ${name}`}
-            />
-            <h5 className="text-3xl font-bold underline">{description}</h5>
+            <div 
+                className={`${selected ? "bg-red-500" : ""}`} 
+                onClick={() => handleTileClick(index)}
+            >
+                <Image
+                    src={`/${name}.jpeg`}
+                    width={100}
+                    height={100}
+                    alt={`Pai Gow Tile: ${name}`}
+                />
+            </div>
+            <h5 className="text-1xl font-bold underline">{description}</h5>
         </>   
     )
 }
--- app/play/page.tsx
+++ app/play/page.tsx
@@ -10,10 +10,20 @@ export default function Page(){
 
     const [numOfHands, setNumOfHands] = useState(1)
     const [hands, setHands] = useState<TileInterface[][]>([])
+    const [selectedTilesIndex, setSelectedTilesIndex] = useState<number[]>([]);
 
     useEffect(() => {
         setHands(dealNewHand(numOfHands));
     }, [])
+
+    const handleTileClick = (index: number): void => {
+        if (selectedTilesIndex.includes(index)) {
+            const remainingTile = selectedTilesIndex.filter((tileIndex: number) => tileIndex !== index);
+            setSelectedTilesIndex(remainingTile);
+        }
+        else if (selectedTilesIndex.length === 2) return;
+        else setSelectedTilesIndex((i: number[]) => [...i, index]);
+    }
     
     return (
         <>
@@ -25,7 +35,10 @@ export default function Page(){
                                     <TileRankCard
                                         key={`${name}-${index}`}
                                         name={name}
+                                        index={index}
                                         description={description}
+                                        selected={selectedTilesIndex.includes(index)}
+                                        handleTileClick={handleTileClick}
                                     />
                                 )
                         })
